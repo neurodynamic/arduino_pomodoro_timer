a80e1fc9124960be5c195643872abbc7b89c6b4a
Mark Stewart <markfstewart@gmail.com>
2014-03-24 12:07:15 -0600

    Adds limbo phase

--- chair_game.ino
+++ chair_game.ino
@@ -1,19 +1,23 @@
 const int sensorPin = 0;
 const int in_chair_indicator_pin = 3;
-const int pomodoro_length = 7;
+const int work_length = 4;
 const int break_length = 4;
+const int limbo_length = 6;
+const int long_break_every_x_breaks = 4;
+const int long_break_length = 8;
 const int RED_PIN = 9;
 const int GREEN_PIN = 10;
 const int BLUE_PIN = 11;
 const int buzzerPin = 5;
 
-enum modes_t {WORK_MODE, BREAK_MODE};
+enum modes_t {WORK_MODE, LIMBO_MODE, BREAK_MODE};
 modes_t mode = WORK_MODE;
 
 
-int lightLevel, high = 0, low = 1023, intervals_since_last_full_second = 0;
-double work_time_elapsed = 0, break_time_elapsed = 0, interval_in_ms = 100.0;
-boolean in_chair;
+int lightLevel, high = 0, low = 1023;//, intervals_since_last_full_second = 0;
+int greenIntensity, blueIntensity, redIntensity;
+double work_time_elapsed = 0, limbo_time_elapsed = 0, break_time_elapsed = 0, interval_in_ms = 1000.0;
+boolean in_chair, first_limbo_alarm_on = true, second_limbo_alarm_on = true;
 
 void setup()
 {
@@ -28,39 +32,68 @@ void setup()
 
 void loop()
 {
-    play_monster_battle(buzzerPin);
-    delay(3000);
-    play_victory(buzzerPin);
-    delay(3000);
-    play_boss_battle(buzzerPin);
-    delay(10000);
-  // change_mode_if_mode_time_has_expired();
+  // test();
+
+  change_mode_if_necessary();
   
-  // lightLevel = analogRead(sensorPin);
-  // in_chair = lightLevel < 670;
+  lightLevel = analogRead(sensorPin);
+  in_chair = lightLevel < 670;
   
-  // set_chair_indicator_led();
+  set_chair_indicator_led();
   
-  // set_time_indicator_led();
+  set_time_indicator_led();
   
-  // tick();
+  tick();
 }
 
+void test(){
+  while(true){
+    play_boss_battle(buzzerPin);
+    delay(3000);
+  }
+}
 
-
-void change_mode_if_mode_time_has_expired()
+void change_mode_if_necessary()
 {
-  if(mode == WORK_MODE && work_time_elapsed >= pomodoro_length)
+  if(mode == WORK_MODE && work_time_elapsed >= work_length)
+  {
+    // reset_timers();
+    mode = LIMBO_MODE;
+    set_time_indicator_led();
+    first_limbo_alarm_on = true;
+    second_limbo_alarm_on = true;
+    play_limbo_level_one(buzzerPin);
+    play_limbo_level_one(buzzerPin);
+  }
+
+  if(mode == LIMBO_MODE && limbo_time_elapsed >= limbo_length/3 && first_limbo_alarm_on)
+  {
+    play_limbo_level_two(buzzerPin);
+    play_limbo_level_two(buzzerPin);
+    first_limbo_alarm_on = false;
+  }
+
+  if(mode == LIMBO_MODE && limbo_time_elapsed >= limbo_length*2/3 && second_limbo_alarm_on)
+  {
+    play_limbo_level_three(buzzerPin);
+    play_limbo_level_three(buzzerPin);
+    second_limbo_alarm_on = false;
+  }
+
+  if(mode == LIMBO_MODE && (limbo_time_elapsed >= limbo_length || in_chair == false))
   {
     reset_timers();
     mode = BREAK_MODE;
+    set_time_indicator_led();
+    play_monster_battle(buzzerPin);
   }
-  
+
   if(mode == BREAK_MODE && break_time_elapsed >= break_length)
   {
-    play_victory(buzzerPin);
     reset_timers();
     mode = WORK_MODE;
+    set_time_indicator_led();
+    play_victory(buzzerPin);
   }
 }
 
@@ -68,34 +101,47 @@ void change_mode_if_mode_time_has_expired()
     void reset_timers()
     {
       work_time_elapsed = 0;
+      limbo_time_elapsed = 0;
       break_time_elapsed = 0;
     }
 
 void set_time_indicator_led()
 {
-  int pomodoro_completion, break_completion, greenIntensity, blueIntensity, redIntensity;
-  
   if(mode == WORK_MODE)
   {
-    blueIntensity = 0;
-    pomodoro_completion = map(work_time_elapsed, 0, pomodoro_length, 0, 255);
-    pomodoro_completion = constrain(pomodoro_completion, 0, 255);
-  
-    redIntensity = pomodoro_completion;
-    greenIntensity = 255 - pomodoro_completion;
+    shade_green_to_red_fade();
+  }
+  else if(mode == LIMBO_MODE)
+  {
+    shade_green_to_red_fade();
   }
   else
   {
-    greenIntensity = 0;
-    break_completion = map(break_time_elapsed, 0, break_length, 0, 255);
-    break_completion = constrain(break_completion, 0, 255);
-  
-    blueIntensity = break_completion;
-    redIntensity = 255 - break_completion;
+    shade_red_to_blue_fade();
   }
 
   write_to_color_led(redIntensity, greenIntensity, blueIntensity);
 }
+
+    void shade_green_to_red_fade()
+    {
+      blueIntensity = 0;
+      int pomodoro_completion = map(work_time_elapsed + limbo_time_elapsed, 0, work_length + (limbo_length/2), 0, 255);
+      pomodoro_completion = constrain(pomodoro_completion, 0, 255);
+    
+      redIntensity = pomodoro_completion;
+      greenIntensity = 255 - pomodoro_completion;
+    }
+
+    void shade_red_to_blue_fade()
+    {
+      greenIntensity = 0;
+      int break_completion = map(break_time_elapsed, 0, break_length, 0, 255);
+      break_completion = constrain(break_completion, 0, 255);
+    
+      blueIntensity = break_completion;
+      redIntensity = 255 - break_completion;
+    }
 
     void write_to_color_led(int redIntensity, int greenIntensity, int blueIntensity)
     {
@@ -119,44 +165,50 @@ void set_chair_indicator_led()
 void tick()
 {
   delay(interval_in_ms);
-  intervals_since_last_full_second = intervals_since_last_full_second + 1;
+  // intervals_since_last_full_second = intervals_since_last_full_second + 1;
   
-  if(clock_is_on_an_exact_second())
-  {
-    intervals_since_last_full_second = 0;
-    print_status();
-  }
+  // if(clock_is_on_an_exact_second())
+  // {
+  //   intervals_since_last_full_second = 0;
+  //   print_status();
+  // }
   
-  record_work_or_break_time_as_appropriate();
+  advance_appropriate_timer();
 }
 
-    boolean clock_is_on_an_exact_second()
-    {
-      if(intervals_since_last_full_second == 1000.0/interval_in_ms)
-      {
-        return true;
-      }
-      else
-      {
-        return false;
-      }
-    }
+    // boolean clock_is_on_an_exact_second()
+    // {
+    //   if(intervals_since_last_full_second == 1000.0/interval_in_ms)
+    //   {
+    //     return true;
+    //   }
+    //   else
+    //   {
+    //     return false;
+    //   }
+    // }
 
-    void print_status()
-    {
-      Serial.print("light level: ");
-      Serial.print(lightLevel);
-      Serial.print("       work time: ");
-      Serial.print(work_time_elapsed);
-      Serial.print("       break time: ");
-      Serial.println(break_time_elapsed); 
-    }
+    // void print_status()
+    // {
+    //   Serial.print("light level: ");
+    //   Serial.print(lightLevel);
+    //   Serial.print("       work time: ");
+    //   Serial.print(work_time_elapsed);
+    //   Serial.print("       break time: ");
+    //   Serial.println(break_time_elapsed); 
+    // }
 
-    void record_work_or_break_time_as_appropriate()
+    void advance_appropriate_timer()
     {
       if(in_chair)
       {
-        work_time_elapsed = work_time_elapsed + interval_in_ms/1000.0;
+        if(mode == LIMBO_MODE){
+          limbo_time_elapsed = limbo_time_elapsed + interval_in_ms/1000.0;
+        }
+        else
+        {
+          work_time_elapsed = work_time_elapsed + interval_in_ms/1000.0;
+        }
       }
       else
       {
--- songs.ino
+++ songs.ino
@@ -1,6 +1,6 @@
+// http://www.rpgamer.com/games/ff/ff7/ff7mid.html
 
-
-void play_victory(int buzzer)
+double play_victory(int buzzer)
 {
    int notes[] = { 
     NOTE_C6, NOTE_C6, NOTE_C6,
@@ -13,46 +13,147 @@ void play_victory(int buzzer)
 
   double beats[] = {0.333,0.333,0.333,1,1,1,0.333,0.333,0.333,2};
   double tempo = 370;
-
-  playSong(buzzer, notes, beats, tempo, 10);
+  int note_count = 10;
+  
+  return playSong(buzzer, notes, beats, tempo, note_count);
 }
 
-void play_monster_battle(int buzzer)
+double play_limbo_level_one(int buzzer)
+{
+  int note_count = 2;
+  int notes[] = { NOTE_AS1, NOTE_CS2 };
+  double beats[] = {0.25,0.25};
+  double tempo = 370;
+  
+  return playSong(buzzer, notes, beats, tempo, note_count);
+}
+double play_limbo_level_two(int buzzer)
+{
+  int note_count = 2;
+  int notes[] = { NOTE_AS2, NOTE_CS3 };
+  double beats[] = {0.25,0.25};
+  double tempo = 370;
+  
+  return playSong(buzzer, notes, beats, tempo, note_count);
+}
+double play_limbo_level_three(int buzzer)
+{
+  int note_count = 2;
+  int notes[] = { NOTE_AS3, NOTE_CS4 };
+  double beats[] = {0.25,0.25};
+  double tempo = 370;
+  
+  return playSong(buzzer, notes, beats, tempo, note_count);
+}
+
+double play_monster_battle(int buzzer)
 {
    int notes[] = { 
     NOTE_F5, NOTE_F5, 
     NOTE_F5, NOTE_F5,
     NOTE_F5, NOTE_F5, 
-    NOTE_F5, NOTE_F5,
-    NOTE_F5, NOTE_F5, 
-    NOTE_C5, NOTE_DS5,
-    NOTE_FS5, NOTE_F5, 
-    NOTE_DS5, NOTE_FS5,
-    NOTE_F5, NOTE_F5, 
-    NOTE_F5, NOTE_F5,
-    NOTE_F5, NOTE_F5, 
-    NOTE_F5, NOTE_F5,
-    NOTE_F5, NOTE_F5, 
-    NOTE_DS5,
-    NOTE_DS5, 
-    NOTE_DS5,
-    NOTE_F5
+    NOTE_F5, NOTE_F5//,
+    // NOTE_F5, NOTE_F5, 
+    // NOTE_C5, NOTE_DS5,
+    // NOTE_FS5, NOTE_F5, 
+    // NOTE_DS5, NOTE_FS5,
+    // NOTE_F5, NOTE_F5, 
+    // NOTE_F5, NOTE_F5,
+    // NOTE_F5, NOTE_F5, 
+    // NOTE_F5, NOTE_F5,
+    // NOTE_F5, NOTE_F5, 
+    // NOTE_DS5,
+    // NOTE_DS5, 
+    // NOTE_DS5,
+    // NOTE_F5
    };
 
   double beats[] = {
-    0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,
-    0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,
-    0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,
-    0.5,0.5,1,1,1,
-    4
+    0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5//,
+    // 0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,
+    // 0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,
+    // 0.5,0.5,1,1,1,
+    // 4
   };
   double tempo = 250;
+  int note_count = 8;//30;
 
-  playSong(buzzer, notes, beats, tempo, 30);
+  return playSong(buzzer, notes, beats, tempo, note_count);
+}
+
+double play_boss_battle(int buzzer)
+{
+   int notes[] = { 
+    NOTE_A2, NOTE_A2, NOTE_A2, NOTE_C3,
+    NOTE_A2, NOTE_A2, NOTE_A2, NOTE_D3,
+    NOTE_A2, NOTE_A2, NOTE_A2, NOTE_DS3,
+    NOTE_D3, NOTE_C3, NOTE_D3, NOTE_C3,
+    NOTE_B2, NOTE_C3, NOTE_B2,
+
+    NOTE_A2, NOTE_A2, NOTE_A2, NOTE_C3,
+    NOTE_A2, NOTE_A2, NOTE_A2, NOTE_D3,
+    NOTE_A2, NOTE_A2, NOTE_A2, NOTE_DS3,
+    NOTE_D3, NOTE_C3, NOTE_D3, NOTE_C3,
+    NOTE_B2, NOTE_C3, NOTE_B2,
+
+    NOTE_C3, NOTE_C3, NOTE_C3, NOTE_DS3,
+    NOTE_C3, NOTE_C3, NOTE_C3, NOTE_F3,
+    NOTE_C3, NOTE_C3, NOTE_C3, NOTE_FS3,
+    NOTE_F3, NOTE_DS3, NOTE_F3, NOTE_DS3, 
+    NOTE_D3, NOTE_DS3, NOTE_D3,
+
+    NOTE_C3, NOTE_C3, NOTE_C3, NOTE_DS3,
+    NOTE_C3, NOTE_C3, NOTE_C3, NOTE_F3,
+    NOTE_C3, NOTE_C3, NOTE_C3, NOTE_FS3,
+    NOTE_F3, NOTE_DS3, NOTE_F3, NOTE_DS3, 
+    NOTE_D3, NOTE_DS3, NOTE_D3,
+
+    NOTE_C5
+    // , NOTE_D5, NOTE_D5, NOTE_D5,
+    // NOTE_C5, NOTE_D5, NOTE_D5, NOTE_D5
+    // NOTE_DS5, NOTE_F5, NOTE_F5, NOTE_F5,
+    // NOTE_DS5, NOTE_F5, NOTE_F5, NOTE_F5,
+    // NOTE_A5
+   };
+
+  double beats[] = {
+    0.33,0.33,0.33,1,
+    0.33,0.33,0.33,1,
+    0.33,0.33,0.33,0.33,
+    0.33,0.33,0.33,0.33,
+    0.33,1,0.33,
+    0.33,0.33,0.33,1,
+    0.33,0.33,0.33,1,
+    0.33,0.33,0.33,0.33,
+    0.33,0.33,0.33,0.33,
+    0.33,1,0.33,
+
+    0.33,0.33,0.33,1,
+    0.33,0.33,0.33,1,
+    0.33,0.33,0.33,0.33,
+    0.33,0.33,0.33,0.33,
+    0.33,1,0.33,
+    0.33,0.33,0.33,1,
+    0.33,0.33,0.33,1,
+    0.33,0.33,0.33,0.33,
+    0.33,0.33,0.33,0.33,
+    0.33,1,0.33,
+
+    8
+    // ,1,1,1,
+    // 5,1,1,1
+    // 4,1,1,1,
+    // 4,1,1,1,
+    // 4
+  };
+  double tempo = 250;
+  int note_count = 77;
+
+  return playSong(buzzer, notes, beats, tempo, note_count);
 }
 
 
-void play_boss_battle(int buzzer)
+double play_monster_battle_ascension(int buzzer)
 {
    int notes[] = { 
     NOTE_C4, NOTE_AS3, NOTE_C4,
@@ -73,18 +174,20 @@ void play_boss_battle(int buzzer)
     1.5,1,1,2
   };
   double tempo = 320;
-
-  playSong(buzzer, notes, beats, tempo, 19);
+  int note_count = 19;
+  
+  return playSong(buzzer, notes, beats, tempo, note_count);
 }
 
-    void playSong(int buzzer, int notes[], double beats[], int tempo, int note_count){
+    double playSong(int buzzer, int notes[], double beats[], int tempo, int note_count){
       int i;
-      double duration;
+      double duration, total_duration = 0;
 
       for (i = 0; i < note_count; i++) // step through the song arrays
       {
         duration = beats[i] * tempo;  // length of note/rest in ms
-        
+        total_duration = total_duration + duration;
+
         if (notes[i] == REST)          // is this a rest? 
         {
           delay(duration);            // then pause for a moment
@@ -96,4 +199,6 @@ void play_boss_battle(int buzzer)
         }
         delay(tempo/10);              // brief pause between notes
       }
+
+      return total_duration;
     }
